ea39672cf1dc6784437dd4ad3bd8ef1ae15e4dbc
Rithik Makhija <rithikmakhija2012@gmail.com>
2020-11-12 20:14:40 +0400

    add product.js file

---
+++ js/products.js
@@ -0,0 +1,110 @@
+let product = [
+    {
+        "id": 1001,
+        "subject": "Computer Science ",
+        "location": "New York",
+        "price": "200",
+        "image": "images/computer science.jpg",
+        "space": 5,
+        "rating": 1,
+        "color": "#F08080"
+    , "quantity":0 },  {
+        "id": 1002,
+        "subject": "English",
+        "location": "Alabama",
+        "price": "399",
+        "image": "images/english (custom).png",
+        "space": 5,
+        "rating": 4,
+        "color": "#00BDDE"
+
+    , "quantity":0 },  {
+        "id": 1003,
+        "subject": "Japanese",
+        "location": "Montreal",
+        "price": "150",
+        "image": "images/Japanese.jpg",
+        "space": 5,
+        "rating": 3,
+        "color": "#125DD2"
+    , "quantity":0 },  {
+        "id": 1004,
+        "subject": "Math",
+        "location": "Toronto",
+        "price": "140",
+        "image": "images/math.jpg",
+        "space": 5,
+        "rating": 1,
+        "color":"#E5C77F"
+    , "quantity":0 },  {
+        "id": 1005,
+        "subject": "History",
+        "location": "Washington DC",
+        "price": "999",
+        "image": "images/history (custom).png",
+        "space": 5,
+        "color": "#f0b7c2",
+        "rating": 1
+    , "quantity":0 }, {
+        "id": 1006,
+        "subject": "Biology",
+        "location": "Boston",
+        "price": "9999",
+        "image": "images/Biology.jpg",
+        "space": 5,
+        "color": "#50EC32",
+        "rating": 1
+    , "quantity":0 }, 
+    {
+        "id": 1007,
+        "subject": "Spanish",
+        "location": "Miami",
+        "price": "9999",
+        "image": "images/spanish (custom).png",
+        "space": 5,"color": "#64dde0",
+        "rating": 1
+    , "quantity":0 }, {
+        "id": 1008,
+        "subject": "French",
+        "location": "Cincinnati",
+        "price": "9999",
+        "image": "images/french (custom).png",
+        "space": 5,"color": "#d43a56",
+        "rating": 1
+    , "quantity":0 }, {
+        "id": 1009,
+        "subject": "Chemistry",
+        "location": "New Jersey",
+        "price": "9999",
+        "image": "images/chemistry (custom).png",
+        "space": 5,"color": "#ff6321",
+        "rating": 1
+    , "quantity":0 }, 
+    {
+        "id": 1010,
+        "subject": "Psychology",
+        "location": "Salem",
+        "price": "9999",
+        "image": "images/psychology (custom).png",
+        "space": 5,"color": "#c090d0",
+        "rating": 1
+    , "quantity":0 }, {
+        "id": 1011,
+        "subject": "Geography",
+        "location": "El Paso",
+        "price": "9999",
+        "image": "images/Geography.jpg",
+        "space": 5,"color": "#eb9411",
+        "rating": 1
+    , "quantity":0 }, {
+        "id": 1012,
+        "subject": "Physics",
+        "location": "Los Angeles",
+        "price": "9999",
+        "image": "images/Physics (custom).png",
+        "space": 5,"color": "#89d417",
+        "rating": 1
+    , "quantity":0 }, 
+
+
+];
